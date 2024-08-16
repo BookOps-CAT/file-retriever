e586c7cc689885bf1be24e8e47a7a3b034ace202
Charlotte Kostelic <charlottekostelic@gmail.com>
2024-08-16 10:51:59 -0400

    removed cli tests from github actions

--- tests/test_file_retriever_cli.py
+++ tests/test_file_retriever_cli.py
@@ -3,12 +3,14 @@ import pytest
 from file_retriever import file_retriever_cli, main
 
 
+@pytest.mark.cli_test
 def test_file_retriever_cli():
     runner = CliRunner()
     runner.invoke(file_retriever_cli)
     assert runner.get_default_prog_name(file_retriever_cli) == "file-retriever-cli"
 
 
+@pytest.mark.cli_test
 def test_file_retriever_cli_get_files(mock_Client, mocker, caplog, mock_config_yaml):
     m = mocker.mock_open(read_data=mock_config_yaml)
     mocker.patch("builtins.open", m)
@@ -23,6 +25,7 @@ def test_file_retriever_cli_get_files(mock_Client, mocker, caplog, mock_config_y
     assert "(FOO) Closing client session" in caplog.text
 
 
+@pytest.mark.cli_test
 def test_file_retriever_cli_get_files_multiple_vendors(
     mock_Client, mocker, caplog, mock_config_yaml
 ):
@@ -45,6 +48,7 @@ def test_file_retriever_cli_get_files_multiple_vendors(
     assert "(BAZ) Closing client session" in caplog.text
 
 
+@pytest.mark.cli_test
 def test_file_retriever_cli_daily_vendor_files(
     mock_Client, mocker, caplog, mock_config_yaml
 ):
@@ -67,6 +71,7 @@ def test_file_retriever_cli_daily_vendor_files(
     assert "(BAZ) Closing client session" in caplog.text
 
 
+@pytest.mark.cli_test
 def test_file_retriever_available_vendors(mocker, mock_config_yaml):
     m = mocker.mock_open(read_data=mock_config_yaml)
     mocker.patch("builtins.open", m)
@@ -78,6 +83,7 @@ def test_file_retriever_available_vendors(mocker, mock_config_yaml):
     assert "Available vendors: ['FOO', 'BAR', 'BAZ', 'NSDROP']" in result.output
 
 
+@pytest.mark.cli_test
 def test_file_retriever_available_vendors_no_vendors(mock_Client, mocker):
     m = mocker.mock_open(read_data="FOO: BAR")
     mocker.patch("builtins.open", m)
@@ -89,6 +95,7 @@ def test_file_retriever_available_vendors_no_vendors(mock_Client, mocker):
     assert "No vendors available." in result.output
 
 
+@pytest.mark.cli_test
 def test_main(mocker):
     mock_main = mocker.Mock()
     mocker.patch("file_retriever.main", return_value=mock_main)
